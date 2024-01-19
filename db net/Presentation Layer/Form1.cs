657b4b0e500ee387f6041d47eac4b919da05305b
Aleksa Kovacevic <48535139+KovacevicAleksa@users.noreply.github.com>
2024-01-19 16:37:29 +0100

    Update Form1.cs

--- db net/Presentation Layer/Form1.cs
+++ db net/Presentation Layer/Form1.cs
@@ -12,42 +12,59 @@ using System.Windows.Forms;
 
 namespace Presentation_Layer
 {
-    // Glavna forma koja prikazuje podatke o studentima.
     public partial class Form1 : Form
     {
-        // Poslovna logika za rad sa studentima.
-        private readonly StudentBussiness studentBussiness;
 
-        // Konstruktor forme.
+        private readonly StudentBussiness studentBussiness;
         public Form1()
         {
-            // Inicijalizacija poslovne logike za rad sa studentima.
             this.studentBussiness = new StudentBussiness();
             InitializeComponent();
         }
 
-        // Event handler koji se poziva kada se promeni selektovani element u listBox-u.
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Implementacija po potrebi.
+
         }
 
-        // Event handler koji se poziva kada se klikne na određeni button ili labelu.
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            // Implementacija po potrebi.
+            Student s = new Student();
+            s.Name = TextBoxName.Text;
+            s.IndexNumber = TextBoxIndx.Text;
+            s.AverageMark = Convert.ToDecimal(TextBoxProsOcena.Text);
+
+            if (this.studentBussiness.InserStudent(s)) {
+
+                refreshData();
+            }
         }
 
-        // Metoda koja osvežava podatke na formi pri njenom učitavanju.
         private void refreshData() 
         {
-            // Dobavljanje liste svih studenata kroz poslovni sloj.
             List<Student> students = this.studentBussiness.GetAllStudent();
-
-            // Čišćenje listBox-a.
             listBoxStudents.Items.Clear();
 
-            // Dodavanje svakog studenta u listBox za prikaz na formi.
             foreach (Student s in students) 
             { 
                 listBoxStudents.Items.Add(s.Id+" "+s.Name+" "+s.IndexNumber+" "+
@@ -55,11 +72,9 @@ namespace Presentation_Layer
             }
         }
 
-        // Event handler koji se poziva kada se forma učita.
-        private void Form1_Load(object sender, EventArgs e)
+        private void TextBoxName_TextChanged(object sender, EventArgs e)
         {
-            // Osvežavanje podataka pri učitavanju forme.
-            refreshData();
+
         }
     }
 }
