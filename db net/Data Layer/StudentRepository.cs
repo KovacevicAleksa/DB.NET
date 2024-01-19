2732dd5ef93858769440b75193d9bf1d179b0e36
Aleksa Kovacevic <48535139+KovacevicAleksa@users.noreply.github.com>
2024-01-19 16:37:55 +0100

    Update StudentRepository.cs

--- db net/Data Layer/StudentRepository.cs
+++ db net/Data Layer/StudentRepository.cs
@@ -8,29 +8,22 @@ using System.Threading.Tasks;
 
 namespace Data_Layer.Models
 {
-    // Klasa koja pruža funkcionalnost za rad sa podacima o studentima.
     public class StudentRepository
     {
-        // Metoda koja vraća listu svih studenata iz baze podataka.
         public List<Student> GetAllStudent() 
         { 
             List<Student> result = new List<Student>();
 
-            // Korišćenje SqlConnection za povezivanje sa bazom podataka.
             using (SqlConnection sqlConnection = new SqlConnection(Constant.connectionString)) 
             {
-                // Kreiranje SQL upita za selektovanje svih studenata.
                 SqlCommand sqlCommand = new SqlCommand();
                 sqlCommand.Connection = sqlConnection;
                 sqlCommand.CommandText = "SELECT * FROM Students";
 
-                // Otvaranje veze sa bazom podataka.
                 sqlConnection.Open();
 
-                // Izvršavanje SQL upita i čitanje rezultata.
                 SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
-                // Čitanje podataka iz rezultata upita i popunjavanje liste studenata.
                 while (sqlDataReader.Read()) 
                 { 
                     Student s = new Student();
@@ -43,25 +36,22 @@ namespace Data_Layer.Models
                 }
             }
 
-            // Vraćanje rezultata.
-            return result;
+                return result;
         }
 
-        // Metoda za unos novog studenta u bazu podataka.
-        // Vraća broj redova koji su promenjeni (trebalo bi da bude 1 ako je unos uspeo).
-        public int InserStudent(Student s) 
+        public int InserStudent(Student s)
         {
-            // Korišćenje SqlConnection za povezivanje sa bazom podataka.
             using (SqlConnection sqlConnection = new SqlConnection(Constant.connectionString))
             {
-                // Kreiranje SQL upita za unos novog studenta.
+
                 SqlCommand sqlCommand = new SqlCommand();
                 sqlCommand.Connection = sqlConnection;
-                sqlCommand.CommandText = 
+                sqlCommand.CommandText =
                     string.Format("INSERT INTO Students VALUES('{0}','{1}',{2})",
-                    s.Name,s.IndexNumber,s.AverageMark);
+                        s.Name, s.IndexNumber, s.AverageMark);
 
-                // Izvršavanje SQL upita za unos i vraćanje broja promenjenih redova.
+                sqlConnection.Open();
+
                 return sqlCommand.ExecuteNonQuery();
             }
         }
