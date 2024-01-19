e5b18b3f6ad1480b0cac0c854b49f58b6a21c0ef
Aleksa Kovacevic <48535139+KovacevicAleksa@users.noreply.github.com>
2024-01-19 13:20:56 +0100

    Update StudentBussiness.cs
    add comment

--- db net/Bussines layer/StudentBussiness.cs
+++ db net/Bussines layer/StudentBussiness.cs
@@ -9,20 +9,25 @@ using System.Runtime.Remoting.Messaging;
 
 namespace Bussines_layer
 {
+    // Poslovni sloj koji upravlja operacijama sa studentima.
     public class StudentBussiness
     {
+        // Repozitorijum za rad sa podacima o studentima.
         private readonly StudentRepository studentRepository;
 
+        // Konstruktor koji inicijalizuje repozitorijum za studente.
         public StudentBussiness() 
         { 
             this.studentRepository = new StudentRepository();
         }
 
+        // Metoda koja vraća sve studente.
         public List<Student> GetAllStudent()
         { 
             return this.studentRepository.GetAllStudent();
         }
 
+        // Metoda za unos novog studenta. Vraća true ako je uspešno unet, inače false.
         public bool InserStudent(Student s) 
         {
             if (this.studentRepository.InserStudent(s) > 0) 
@@ -31,10 +36,12 @@ namespace Bussines_layer
             }
             return false;
         }
+
+        // Metoda koja vraća listu studenata sa ocenom iznad prosleđenog proseka.
         public List<Student> GetStudentsGTAvgMark(decimal average) 
         {
             return this.studentRepository.GetAllStudent()
-                .Where(s => s.AverageMark > s.AverageMark).ToList();
+                .Where(s => s.AverageMark > average).ToList();
         }
     }
 }
